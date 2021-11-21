6b51a5270220a426d1f57df7d4fa04b5f0f9ddea
RubsRz <rodriguezperezrubenalejandro@gmail.com>
2021-11-21 14:25:11 -0600

    nuevos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase_config: {
+    apiKey: "AIzaSyD3Jrecs6MI3rdFPccJto3COYfT3dNTSNY",
+    authDomain: "login-register-af8b4.firebaseapp.com",
+    databaseURL: "https://login-register-af8b4-default-rtdb.firebaseio.com",
+    projectId: "login-register-af8b4",
+    storageBucket: "login-register-af8b4.appspot.com",
+    messagingSenderId: "58818495553",
+    appId: "1:58818495553:web:de95f8ff1f82b01d5f32c0"
+  }
 };
